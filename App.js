1d90c56800bcd2ec7b15f57b3b53cc44f7f435aa
Rowina San Juan <rowinasanjuan@Rowinas-MacBook-Air.local>
2025-01-16 19:55:43 +0200

    lisätty toiminnallisuuksille tyylit

--- App.js
+++ App.js
@@ -1,25 +1,37 @@
 import { useState } from 'react';
-import { Alert, Button, StyleSheet, Text, View, TextInput } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 
 export default function App() {
 
   const [text, setText] = useState("");
 
+  const Painike = ({ onPress, title }) => (
+    <TouchableOpacity onPress={onPress} style={styles.buttonContainer}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
 
-  const handlePress = () => {
-    Alert.alert("You typed: " + text);
-  };
 
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 18 }}>Result: </Text>
+      <Text style={{ fontSize: 18, marginBottom: 20 }}>Result: </Text>
 
-      <TextInput
-        placeholder='Ensimmäinen luku'
-        onChangeText={text => setText(text)}
-        value={text} />
+      <View style={styles.textInputs}>
+        <TextInput
+          placeholder='Ensimmäinen luku'
+          onChangeText={text => setText(text)}
+          value={text} style={styles.input} />
 
-      <Button onPress={handlePress} title="Press me" />
+        <TextInput
+          placeholder='Toinen luku'
+          onChangeText={text => setText(text)}
+          value={text} style={styles.input} />
+      </View>
+
+      <View style={styles.buttons}>
+        <Painike title="+" backgroundColor="#007bff" />
+        <Painike title="-" backgroundColor="#007bff" />
+      </View>
     </View>
 
   )
@@ -31,6 +43,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+  },
+  textInputs: {
+    marginBottom: 20,
+  },
+  input: {
+    borderColoer: '#000',
+    borderWidth: 0.5,
+    marginBottom: 10,
+    padding: 8,
+  },
+  buttons: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    width: '25%',
+  },
+  buttonContainer: {
+    backgroundColor: '#009688',
+    paddingHorizontal: 10,
+    paddingVertical: 10
+  },
+  buttonText: {
+    color: '#fff'
   }
 });
